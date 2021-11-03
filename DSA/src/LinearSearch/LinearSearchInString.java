62643c26b45dbba89d99f2ba5c42bf451cb815b5
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-11-03 22:48:28 +0530

    Linear Search In String - For Each | DSA - 1

--- DSA/src/LinearSearch/LinearSearchInString.java
+++ DSA/src/LinearSearch/LinearSearchInString.java
@@ -1,12 +1,16 @@
 package LinearSearch;
 
+import java.util.Arrays;
+
 public class LinearSearchInString {
 
     public static void main(String[] args) {
         String name = "Mausam";
         char target = 's';
 
-        System.out.println(linearSearchInString(name, target));
+        System.out.println(linearSearchInStringForEach(name, target));
+
+        // System.out.println(Arrays.toString(name.toCharArray()));
     }
 
 
@@ -16,9 +20,27 @@ public class LinearSearchInString {
         // if length of string is zero : return false
         if (name.length() == 0) return false;
 
-        // run a loop by converting string into character array : character found then return true
+        // run a loop  : character found then return true
         for (int i = 0; i < name.length(); i++) {
             if (name.charAt(i) == target) return true;
+        }
+
+        // character not found
+        return false;
+    }
+
+    /*
+    2. Using for each loop
+     */
+
+    static boolean linearSearchInStringForEach(String name, char target) {
+
+        // if length of string is zero : return false
+        if (name.length() == 0) return false;
+
+        // run a for each loop, convert the string in to char array
+        for (char ch : name.toCharArray()) {
+            if (ch == target) return true;
         }
 
         // character not found
