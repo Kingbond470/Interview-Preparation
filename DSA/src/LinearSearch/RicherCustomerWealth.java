81605353a1db672a98b367977c745a10aa8f1678
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-11-06 13:17:34 +0530

    Linear Search - Richest Customer Wealth | DSA - 2

--- DSA/src/LinearSearch/RicherCustomerWealth.java
+++ DSA/src/LinearSearch/RicherCustomerWealth.java
@@ -4,9 +4,9 @@ public class RicherCustomerWealth {
 
     public static void main(String[] args) {
         int[][] arr = {
-                {1,5},
-                {7,3},
-                {5,3},
+                {1, 5},
+                {7, 3},
+                {5, 3},
         };
 
         System.out.println(richestCustomer(arr));
@@ -14,12 +14,22 @@ public class RicherCustomerWealth {
 
     static int richestCustomer(int[][] arr) {
         int max = Integer.MIN_VALUE;
-
+/*
         for (int customer = 0; customer < arr.length; customer++) {
 
             int csum = 0;
             for (int accountant = 0; accountant < arr[customer].length; accountant++) {
                 csum = csum + arr[customer][accountant];
+            }
+            max = Math.max(csum, max);
+        }
+
+ */
+        for (int[] customer : arr) {
+
+            int csum = 0;
+            for (int accountant : customer) {
+                csum = csum + accountant;
             }
             max = Math.max(csum, max);
         }
