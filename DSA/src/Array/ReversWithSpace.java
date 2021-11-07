8fb95632746b3f7542833bf4376a690aabe5e241
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-11-07 18:19:55 +0530

    Reverse With Space | DSA - 3

--- DSA/src/Array/ReversWithSpace.java
+++ DSA/src/Array/ReversWithSpace.java
@@ -3,10 +3,13 @@ package Array;
 public class ReversWithSpace {
 
     public static void main(String[] args) {
-        String str = "Mausam Singh";
+        String str = "You are awesome value";
         char[] arr = str.toCharArray();
 
-        reversNameWithSpace(arr);
+
+        //  reversNameWithSpace(arr);
+        // reversePrint(arr);
+        reversePrint1(arr);
     }
     /*
 
@@ -49,8 +52,47 @@ public class ReversWithSpace {
     }
 
 
+    // brute force
+    static void reversePrint(char[] arr) {
+
+        String res = "";
+        for (char element : arr) res += element;
+        //System.out.println(res);
+
+        String[] res1 = res.split(" ");
+        for (int i = res1.length - 1; i >= 0; i--) System.out.print(res1[i] + " ");
+
+    }
+
+    static void reversePrint1(char[] arr) {
+
+        String res = "";
+        for (char element : arr) res += element;
+        //System.out.println(res);
+        int cur = 0, end = arr.length - 1;
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                cur = 0;
+                System.out.print(res.substring(cur, end + 1));
+            }
+
+            if (arr[i] == ' ') {
+                cur = i + 1;
+                System.out.print(res.substring(cur, end == arr.length - 1 ? end + 1 : end) + " ");
+                end = cur - 1;
+            }
+        }
+
+
+    }
+
+
 }
 /*
 Input - Mausam Singh
- output - Singh Mausam
+output - Singh Mausam
+T(c) - O(n*n)
+S(c) - O(1)
  */
+
