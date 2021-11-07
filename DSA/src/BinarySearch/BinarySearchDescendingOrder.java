74395929cfaee6d3d02edf8516985e94e2d8cbcb
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-11-07 11:13:45 +0530

    Binary Search Descending Order  -  | DSA - 3

--- DSA/src/BinarySearch/BinarySearchDescendingOrder.java
+++ DSA/src/BinarySearch/BinarySearchDescendingOrder.java
@@ -4,9 +4,10 @@ public class BinarySearchDescendingOrder {
 
     public static void main(String[] args) {
         int[] arr = {90, 80, 20, 10, 5, 1};
-        int target=90;
+        int target = 80;
 
         System.out.println(binarySearch(arr, target));
+        System.out.println(binarySearchFound(arr, target));
     }
 
     // search the target element in array : return the index
@@ -25,6 +26,26 @@ public class BinarySearchDescendingOrder {
 
         // if target element did not found : return -1
         return -1;
+    }
+
+    // search the target element : return true or false
+    static boolean binarySearchFound(int[] arr, int target) {
+        int start = 0, end = arr.length - 1;
+
+        // finding mid index
+
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) start = mid + 1;
+            else if (target > arr[mid]) end = mid - 1;
+            else return true;
+        }
+
+        // element did not found
+        return false;
+
     }
 
 
